6888d439037c2ba3d5572aa8bfe76e7017352536
Sergio <48896025+Sergio3215@users.noreply.github.com>
2019-06-30 14:08:13 -0300

    Add files via upload

---
+++ WindowsFormsApp6/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace WindowsFormsApp6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtfecha = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtfecha
+            // 
+            this.txtfecha.Enabled = false;
+            this.txtfecha.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtfecha.Location = new System.Drawing.Point(70, 97);
+            this.txtfecha.Name = "txtfecha";
+            this.txtfecha.Size = new System.Drawing.Size(332, 38);
+            this.txtfecha.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.Control;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(12, 189);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(203, 44);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Capturar Fecha";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Control;
+            this.label1.Location = new System.Drawing.Point(106, 63);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(277, 31);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Se cambio la fecha?";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.Control;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(293, 189);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(219, 44);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Comprobar Fecha";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Maroon;
+            this.ClientSize = new System.Drawing.Size(524, 261);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtfecha);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtfecha;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ WindowsFormsApp6/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Capturar()
+        {
+            File.WriteAllText("fecha.text", DateTime.Now.ToString("dd/MM/yyyy"));
+        }
+
+        private void comprobar()
+        {
+            try
+            {
+                string dia = "", mes = "", año = "";
+                string[] corte;
+
+                bool error = false;
+
+                corte = File.ReadAllText("fecha.text").Split('/');
+                dia = corte[0];
+                mes = corte[1];
+                año = corte[2];
+
+                File.WriteAllText("fecha1.text",DateTime.Now.ToString("dd/MM/yyyy"));
+
+                string[] corte1 = File.ReadAllText("fecha1.text").Split('/');
+
+                if (int.Parse(corte1[2]) >= int.Parse(año))
+                {
+                    if (int.Parse(corte1[1]) == int.Parse(mes))
+                    {
+                        if (int.Parse(corte1[0]) >= int.Parse(dia))
+                        {
+                            txtfecha.Text = "La hora esta correcta";
+                        }
+                        else
+                            error = true;
+                    }
+                    else
+                        error = true;
+
+                    if (int.Parse(corte1[1]) > int.Parse(mes))
+                    {
+                        txtfecha.Text = "La hora esta correcta";
+                        error = false;
+                    }
+
+
+                }
+                else
+                    error = true;
+
+                if(error == true)
+                {
+                    txtfecha.Text = "Se retraso la fecha";
+                }
+            }
+            catch
+            {
+
+            }
+            finally
+            {
+                File.Delete("fecha1.text");
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Capturar();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            comprobar();
+        }
+    }
+}
